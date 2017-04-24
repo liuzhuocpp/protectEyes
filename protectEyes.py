2fca1379f72bcb276709f67efb0b8e826730b83c
liuzhuocpp <liuzhuocpp@qq.com>
2017-04-24 16:33:35 +0800

    add print info every minutes

--- protectEyes.py
+++ protectEyes.py
@@ -11,27 +11,41 @@ user32 = ctypes.windll.User32
 
 
 def waitUntilUnlock():
-    time.sleep(1)
-    OpenDesktop = user32.OpenDesktopA
-    SwitchDesktop = user32.SwitchDesktop
-    DESKTOP_SWITCHDESKTOP = 0x0100
-    while 1:
-      hDesktop = OpenDesktop ("default", 0, False, DESKTOP_SWITCHDESKTOP)
-      result = SwitchDesktop (hDesktop)
-      if result:
-        print "Unlocked"
-        break
-      else:
-        print time.asctime (), "still locked"
-        time.sleep (2)
+  time.sleep(1)
+  OpenDesktop = user32.OpenDesktopA
+  SwitchDesktop = user32.SwitchDesktop
+  DESKTOP_SWITCHDESKTOP = 0x0100
+  while 1:
+    hDesktop = OpenDesktop ("default", 0, False, DESKTOP_SWITCHDESKTOP)
+    result = SwitchDesktop (hDesktop)
+    if result:
+      print "Unlocked"
+      break
+    else:
+      print time.asctime (), "still locked"
+      time.sleep (2)
 
-    
+
+
+def waitDuration(duration):  
+  printDuration = 60
+  printNumber = duration / printDuration
+  while printNumber > 0:
+    print "After " + str(printNumber * printDuration / 60) + " minutes, your pc will be locked"
+    printNumber -= 1
+    time.sleep(printDuration)
+
+
+
+
 duration = 30 * 60 # 20 minute
-# duration = 5
-while True:
-    time.sleep(duration)    
-    user32.LockWorkStation()
 
+# duration = 5
+while True:   
+       
+    waitDuration(duration)
+    user32.LockWorkStation()
     waitUntilUnlock()
+
 
 
